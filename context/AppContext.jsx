e7e9514efb528d712196e275ebac20f63ef62db4
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2025-10-24 19:16:10 +0530

    first Commit

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -2,6 +2,7 @@
 import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
 
 export const AppContext = createContext();
 
@@ -14,6 +15,7 @@ export const AppContextProvider = (props) => {
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
 
+    const {user} = useUser()
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
     const [isSeller, setIsSeller] = useState(true)
@@ -82,6 +84,7 @@ export const AppContextProvider = (props) => {
     }, [])
 
     const value = {
+        user,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
---
+++ models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    _id:{
+        type: String,
+        required: true
+    },
+    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    cartItems:{
+        type: Object,
+        default: {}
+    }
+}, { minimize: false });
+
+const User = mongoose.model || mongoose.model('User', UserSchema);
+
+
+export default User;
